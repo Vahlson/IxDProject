13dc7152fcc9a1b60fed600a7c1abae2e27ea7fa
eelinnilssson <elnilss@student.chalmers.se>
2022-10-10 16:43:46 +0200

    Solved bug when changing direction

--- Assets/Scripts/ChangePlayer.cs
+++ Assets/Scripts/ChangePlayer.cs
@@ -15,12 +15,15 @@ public class ChangePlayer : MonoBehaviour
     int order;
     int fitList;
     int restartList;
+    int firstElement;
+    int orderLeft;
 
     public Sprite[] nextImg;
     // Start is called before the first frame update
     void Start()
     {
         order = 0;
+        firstElement = 0;
 
     }
 
@@ -32,12 +35,19 @@ public class ChangePlayer : MonoBehaviour
 
     public void changeSpriteLeft()
     {
-        order++;
+        if (firstElement == 1)
+        {
+            order = 1;
+            firstElement = 0;
+
+        }
+        order--;
+        orderLeft = Mathf.Abs(order);
 
 
-        startLeft.sprite = nextImg[order];
+        startLeft.sprite = nextImg[orderLeft];
 
-        if (order > nextImg.Length - 2)
+        if (orderLeft > nextImg.Length - 2)
         {
             print("2");
             startSelected.sprite = nextImg[0];
@@ -45,24 +55,24 @@ public class ChangePlayer : MonoBehaviour
         }
 
 
-        else if (order > nextImg.Length - 3)
+        else if (orderLeft > nextImg.Length - 3)
         {
             print("first");
-            startSelected.sprite = nextImg[order + 1];
+            startSelected.sprite = nextImg[orderLeft + 1];
             startRight.sprite = nextImg[0];
         }
         else
         {
             print("3");
 
-            startSelected.sprite = nextImg[order + 1];
-            startRight.sprite = nextImg[order + 2];
+            startSelected.sprite = nextImg[orderLeft + 1];
+            startRight.sprite = nextImg[orderLeft + 2];
 
         }
 
-        if (order == (nextImg.Length - 1))
+        if (Mathf.Abs(order) == (nextImg.Length - 1))
         {
-            order = -1;
+            firstElement = 1;
         }
 
 
@@ -70,6 +80,12 @@ public class ChangePlayer : MonoBehaviour
 
     public void changeSpriteRight()
     {
+
+        if (firstElement == 1)
+        {
+            order = -1;
+            firstElement = 0;
+        }
         order++;
 
 
@@ -102,6 +118,11 @@ public class ChangePlayer : MonoBehaviour
             startLeft.sprite = nextImg[3];
             startSelected.sprite = nextImg[0];
             startRight.sprite = nextImg[1];
+        }
+
+        if (Mathf.Abs(order) == (nextImg.Length - 1))
+        {
+            firstElement = 1;
         }
 
 
