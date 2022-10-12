6ca731bfcdd1d5eea0610de02ca9304e4b37e242
eelinnilssson <elnilss@student.chalmers.se>
2022-10-12 19:38:01 +0200

    MAin menu added

---
+++ Assets/Scripts/CharacterSelecting.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class HoldData
+{
+    public static int selectedCharacter { get; set; }
+}
+
+public class CharacterSelecting : MonoBehaviour
+{
+    // public static int selecedCharacter = 0;
+
+    public GameObject[] characters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideAllPlayers();
+        HoldData.selectedCharacter = 0;
+        characters[HoldData.selectedCharacter].SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void hideAllPlayers()
+    {
+        foreach (GameObject g in characters)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void nextCharacter()
+    {
+        characters[HoldData.selectedCharacter].SetActive(false);
+        HoldData.selectedCharacter++;
+        if (HoldData.selectedCharacter >= characters.Length)
+        {
+            HoldData.selectedCharacter = 0;
+        }
+
+        characters[HoldData.selectedCharacter].SetActive(true);
+
+    }
+
+    public void prevCharacter()
+    {
+        characters[HoldData.selectedCharacter].SetActive(false);
+        HoldData.selectedCharacter--;
+        if (HoldData.selectedCharacter < 0)
+        {
+            HoldData.selectedCharacter = characters.Length - 1;
+        }
+
+        characters[HoldData.selectedCharacter].SetActive(true);
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/SetPlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlayer : MonoBehaviour
+{
+
+    public GameObject[] playerCharacters;
+    private CharacterSelecting characterSelecting;
+    private int chosenPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideCharactersNotUSed();
+        playerCharacters[HoldData.selectedCharacter].SetActive(true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void hideCharactersNotUSed()
+    {
+
+        print(playerCharacters);
+        foreach (GameObject child in playerCharacters)
+        {
+            child.SetActive(false);
+
+        }
+
+    }
+}
