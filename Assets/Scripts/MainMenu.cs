2f9b02c82c8fa9967780601e9ff87d1a67530405
Alexander Larsson Vahlberg <Legetic@hotmail.com>
2022-10-15 13:13:34 +0200

    Improved Main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,10 +8,10 @@ public class MainMenu : MonoBehaviour
     public string firstLevel;
 
     private ArduinoInputController arduinoInputController;
-    [SerializeField]
-    private Image _progressBar;
-    [SerializeField]
-    private Image _progressIndicator;
+    //[SerializeField]
+    //private Image _progressBar;
+    /* [SerializeField]
+    private Image _progressIndicator; */
     private float _stepSize;
     public int nSteps = 10;
     public int nSeconds = 10;
@@ -19,24 +19,35 @@ public class MainMenu : MonoBehaviour
     private int currentSteps = 0;
     private float _endpos;
 
+
+    private float smoothCurrentStep;
+    [SerializeField] private float currentStepDecreaseSpeed = 0.1f;
+
+    [SerializeField] Transform progressBar;
+    [SerializeField] RectTransform progressBarImage;
+    [SerializeField] Transform runningIcon;
+
     [SerializeField] private CharacterSelecting characterSelecting;
 
     void Start()
     {
         this.arduinoInputController = GetComponent<ArduinoInputController>();
-        _endpos = _progressBar.rectTransform.rect.width - _progressIndicator.rectTransform.rect.width;
+        //_endpos = _progressBar.rectTransform.rect.width - _progressIndicator.rectTransform.rect.width;
         _stepSize = _endpos / nSteps;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        smoothCurrentStep -= Time.deltaTime * currentStepDecreaseSpeed;
+        smoothCurrentStep = Mathf.Clamp(smoothCurrentStep, 0f, 1f);
 
         if (arduinoInputController.getKeyDown(1) || arduinoInputController.getKeyDown(2))
         {
             steps.Enqueue(Time.realtimeSinceStartup);
             currentSteps++;
+
+            smoothCurrentStep += 0.1f;
         }
 
         if (characterSelecting != null)
@@ -73,12 +84,24 @@ public class MainMenu : MonoBehaviour
             }
         }
 
+
+
         if (Input.GetKeyDown("h") || Input.GetKeyDown("j"))
         {
             steps.Enqueue(Time.realtimeSinceStartup);
             currentSteps++;
+
+            smoothCurrentStep += 0.1f;
         }
-        while (steps.Count > 0 && Time.realtimeSinceStartup - steps.Peek() >= nSeconds)
+
+        progressBar.localScale = new Vector3(smoothCurrentStep, progressBar.localScale.y, progressBar.localScale.z);
+        runningIcon.localPosition = new Vector3(progressBar.localScale.x * progressBarImage.rect.width + 100, runningIcon.localPosition.y, runningIcon.localPosition.z);
+        if (smoothCurrentStep >= 1)
+        {
+            startGame();
+        }
+
+        /* while (steps.Count > 0 && Time.realtimeSinceStartup - steps.Peek() >= nSeconds)
         {
             steps.Dequeue();
             currentSteps--;
@@ -88,7 +111,7 @@ public class MainMenu : MonoBehaviour
         if (currentSteps >= 10)
         {
             startGame();
-        }
+        } */
     }
 
     public void startGame()
